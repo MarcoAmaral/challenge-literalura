41d5bb72fe6e7ec9cb3eb47cf0b122564e21c4d3
Marco Amaral <marco.amaral@gmail.com>
2025-01-07 23:22:03 -0300

    Main  Class Added

--- src/main/java/com/br/marco/literalura/LiteraluraApplication.java
+++ src/main/java/com/br/marco/literalura/LiteraluraApplication.java
@@ -1,14 +1,24 @@
 package com.br.marco.literalura;
 
+import com.br.marco.literalura.main.Main;
+import com.br.marco.literalura.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+		public static void main(String[] args) {
+				SpringApplication.run(LiteraluraApplication.class, args);
+		}
 
-	public static void main(String[] args) {
+		@Autowired
+		private BookRepository repository;
 
-			SpringApplication.run(LiteraluraApplication.class, args);
-	}
-
+		@Override
+		public void run(String... args) throws Exception {
+				Main main = new Main(repository);
+				main.displayMenu();
+		}
 }
--- src/main/java/com/br/marco/literalura/model/Author.java
+++ src/main/java/com/br/marco/literalura/model/Author.java
@@ -4,75 +4,72 @@ import jakarta.persistence.*;
 
 import java.util.List;
 
+@Entity
+@Table(name = "autores")
 public class Author {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		@Column(name = "nome", unique = true)
+		private String name;
+		@Column(name = "ano_nascimento")
+		private Integer birthYear;
+		@Column(name = "ano_morte")
+		private Integer deathYear;
 
-		@Entity
-		@Table(name = "autores")
-		public class Author {
-				@Id
-				@GeneratedValue(strategy = GenerationType.IDENTITY)
-				private Long id;
-				@Column(name = "nome", unique = true)
-				private String name;
-				@Column(name = "ano_nascimento")
-				private Integer birthYear;
-				@Column(name = "ano_morte")
-				private Integer deathYear;
+		@ManyToMany(mappedBy = "authors", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+		private List<Book> books;
 
-				@ManyToMany(mappedBy = "authors", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-				private List<Book> books;
+		public Author() {}
 
-				public Author() {}
+		public Author(String name, Integer birthYear, Integer deathYear) {
+				this.name = name;
+				this.birthYear = birthYear;
+				this.deathYear = deathYear;
+		}
 
-				public Author(String name, Integer birthYear, Integer deathYear) {
-						this.name = name;
-						this.birthYear = birthYear;
-						this.deathYear = deathYear;
-				}
+		public String getName() {
+				return name;
+		}
 
-				public String getName() {
-						return name;
-				}
+		public void setName(String name) {
+				this.name = name;
+		}
 
-				public void setName(String name) {
-						this.name = name;
-				}
+		public Integer getBirthYear() {
+				return birthYear;
+		}
 
-				public Integer getBirthYear() {
-						return birthYear;
-				}
+		public void setBirthYear(Integer birthYear) {
+				this.birthYear = birthYear;
+		}
 
-				public void setBirthYear(Integer birthYear) {
-						this.birthYear = birthYear;
-				}
+		public Integer getDeathYear() {
+				return deathYear;
+		}
 
-				public Integer getDeathYear() {
-						return deathYear;
-				}
+		public void setDeathYear(Integer deathYear) {
+				this.deathYear = deathYear;
+		}
 
-				public void setDeathYear(Integer deathYear) {
-						this.deathYear = deathYear;
-				}
+		public List<Book> getBooks() {
+				return books;
+		}
 
-				public List<Book> getBooks() {
-						return books;
-				}
+		public void setBooks(List<Book> books) {
+				this.books = books;
+		}
 
-				public void setBooks(List<Book> books) {
-						this.books = books;
-				}
-
-				@Override
-				public String toString() {
-						return """
+		@Override
+		public String toString() {
+				return """
                     ------ AUTOR ------
                     Nome: %s
                     Ano de Nascimento: %s
                     Ano da Morte: %s
                     -------------------
                     """.formatted(this.getName(),
-										this.getBirthYear() == null ? "N/A" : this.getBirthYear(),
-										this.getDeathYear() == null ? "N/A" : this.getDeathYear());
-				}
+								this.getBirthYear() == null ? "N/A" : this.getBirthYear(),
+								this.getDeathYear() == null ? "N/A" : this.getDeathYear());
 		}
 }
