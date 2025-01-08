82295ec957170f3071d08515d0a8a3e1b68b1326
Marco Amaral <marco.amaral@gmail.com>
2025-01-07 22:38:26 -0300

    AuthorData Class Added

--- src/main/java/com/br/marco/literalura/model/AuthorData.java
+++ src/main/java/com/br/marco/literalura/model/AuthorData.java
@@ -1,4 +1,8 @@
 package com.br.marco.literalura.model;
 
-public class AuthorData {
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record AuthorData(String name,
+												 @JsonAlias("birth_year") Integer birthYear,
+												 @JsonAlias("death_year") Integer deathYear) {
 }
